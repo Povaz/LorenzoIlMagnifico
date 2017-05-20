eb22e60a9c1e5fbfbf0278fe853ccf635396db7c
Paolo Trilli <paolo.trilli@mail.polimi.it>
2017-05-20 15:37:54 +0200

    JSONTerritoryCard tested

--- src/test/java/it/polimi/ingsw/pcXX/JSONUtilityTest.java
+++ src/test/java/it/polimi/ingsw/pcXX/JSONUtilityTest.java
@@ -35,4 +35,23 @@ public class JSONUtilityTest extends TestCase{
 
         assertEquals(expected, calculated);
     }
+
+    public void testJSONimportTerritoryCard2(){
+        Set<Reward> fastRewards = new HashSet<Reward>();
+        fastRewards.add(new Point(PointType.MILITARY_POINT, 2));
+        fastRewards.add(new Resource(ResourceType.SERVANT, 1));
+        Set<Reward> earnings = new HashSet<Reward>();
+        earnings.add(new Point(PointType.MILITARY_POINT, 1));
+        earnings.add(new Resource(ResourceType.SERVANT, 2));
+        TerritoryCard expected = new TerritoryCard("Città Fortificata", 3, fastRewards, 2, earnings);
+
+        TerritoryCard calculated;
+        try {
+            calculated = (TerritoryCard) JSONUtility.getCard(3, 7, CardType.TERRITORY);
+        } catch(Exception e){
+            calculated = null;
+        }
+
+        assertEquals(expected, calculated);
+    }
 }
