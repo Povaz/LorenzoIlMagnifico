f3d810b847a23d5c968b0a192d3ff448f8edbd20
Lacieoz <tommaso.tesser@mail.polimi.it>
2017-05-24 16:36:59 +0200

    modifiche a client

--- src/main/java/it/polimi/ingsw/pcXX/Socket/Client.java
+++ src/main/java/it/polimi/ingsw/pcXX/Socket/Client.java
@@ -3,42 +3,94 @@ package it.polimi.ingsw.pcXX.Socket;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Client {
-	private String name;
-	private String ip;
-	private int port;
+	private String username;
+	private final String ip = "127.0.0.1";
+	private final int port = 1337;
+	private static Socket socketServer;
 	
-	public Client(String name, String ip, int port) {
-		this.name = name;
-		this.ip = ip;
-		this.port = port;
+	public Client() throws UnknownHostException, IOException {
+		socketServer = new Socket (ip, port);
+	}
+	
+	public void setName(String username){
+		this.username=username;
 	}
 	
 	public void startClient() throws IOException {
-		Socket socket = new Socket(ip, port);
-		System.out.println("Connection established");
-		System.out.println("Attesa inizio partita");
-		PrintWriter socketOut = new PrintWriter(socket.getOutputStream());
-		socketOut.println(name);
-		socketOut.flush();
-		
+		System.out.println("Attesa Inizio Game");
 		
 		//sempre pronto a ricevere notifiche
-		Scanner notify = new Scanner(socket.getInputStream());
 		while (true){
-			String line = notify.nextLine();
+			String line = receiveFromServer();
 			System.out.println(line);
+			if(line.equals("Inizio Game")){
+				break;
+			}
 		}
-	}	
+		notify.close();
+		return;
+	}
 	
-	public static void main(String[] args) {
-		//chiede nome e inizializza
+	public static void sendToServer(String message) throws IOException{
+		PrintWriter socketOut = new PrintWriter(socketServer.getOutputStream());
+		socketOut.println(message);
+		socketOut.flush();
+	}
+	
+	public static String receiveFromServer() throws IOException{
+		Scanner socketIn = new Scanner(socketServer.getInputStream());
+		String received = socketIn.nextLine();
+		socketIn.close();
+		return received;
+	}
+	
+	private static String login() throws IOException{
+		System.out.println("LOGIN:");
 		Scanner in = new Scanner(System.in);
-		System.out.println("nome UserLoginImpl?");
-		String name = in.nextLine();
-		Client client = new Client(name, "127.0.0.1", 1337);
+		String username;
+		String password;
+		
+		System.out.println("Username : ");	
+		username = in.nextLine();
+		sendToServer(username);
+		
+		while(true){
+			
+			System.out.println("Password : ");
+			password = in.nextLine();
+			sendToServer(password);
+		
+			if(receiveFromServer().equals("confirmed")){
+				System.out.println("Bentornato " + username + "!");
+				break;
+			}
+			else if (receiveFromServer().equals("wrong password")){
+				System.out.println("Password sbagliata, ritenta!");
+			}
+			else{
+				System.out.println("Nuovo Utente registrato! Benvenuto " + username + "!");
+				break;
+			}
+		}
+		in.close();
+		return username;
+	}
+	
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		
+		//Fa il login
+		
+		Client client = new Client();
+		
+		System.out.println("Connection established");
+		System.out.println("");
+		
+		client.setName(login());
+		
 		try {
 			client.startClient();
 		}
