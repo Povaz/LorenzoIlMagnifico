f8bec8e6e8a2c5d1b324d141bb7901f88ff4f1df
Povaz <erick.venneri@mail.polimi.it>
2017-06-03 14:23:51 +0200

    LeaderCard Commit 1: Abstract Class defined

--- src/main/java/it/polimi/ingsw/pcXX/LeaderCard.java
+++ src/main/java/it/polimi/ingsw/pcXX/LeaderCard.java
@@ -1,7 +1,62 @@
 package it.polimi.ingsw.pcXX;
 
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Created by trill on 24/05/2017.
  */
-public class LeaderCard {
+public abstract class LeaderCard {
+    private final String name;
+    private final boolean inHand;
+    private final Set<Reward> activationRewardRequirement;
+    private final Map<CardType, Integer> activationCardTypeRequirement;
+
+
+    public LeaderCard (String name, boolean inHand, Set<Reward> activationRewardRequirement, Map<CardType, Integer> activationCardTypeRequirement) {
+        this.name = name;
+        this.inHand = inHand;
+        this.activationRewardRequirement = activationRewardRequirement;
+        this.activationCardTypeRequirement = activationCardTypeRequirement;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isInHand() {
+        return inHand;
+    }
+
+    public Set<Reward> getActivationRewardRequirement() {
+        return activationRewardRequirement;
+    }
+
+    public Map<CardType, Integer> getActivationCardTypeRequirement() {
+        return activationCardTypeRequirement;
+    }
+
+    @Override
+    public String toString () {
+        String leaderCardString = "";
+
+        leaderCardString += "Name: " + name + "\n";
+        leaderCardString += "Hand: " + inHand + "\n";
+
+        if (activationRewardRequirement != null) {
+            leaderCardString += "Rewards Requirement: \n";
+            for (Reward r: activationRewardRequirement) {
+                leaderCardString += "  " + r.toString() + "\n";
+            }
+        }
+
+        if (activationCardTypeRequirement != null) {
+            leaderCardString += "Cards Requirement: \n";
+            for (Map.Entry <CardType, Integer> entry : activationCardTypeRequirement.entrySet()) {
+                leaderCardString += "CardType: " + entry.getKey() + "Count: " + entry.getValue();
+            }
+        }
+        return leaderCardString;
+    }
 }
+
