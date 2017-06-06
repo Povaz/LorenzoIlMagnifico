7c55ca74228bb700852f46539405d9a0bfe9c7cf
Povaz <erick.venneri@mail.polimi.it>
2017-06-06 14:58:03 +0200

    Temporary Class Terminal Input created

---
+++ src/main/java/it/polimi/ingsw/pcXX/IllegalCouncilPrivilegeQuantity.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.pcXX;
+
+/**
+ * Created by Povaz on 06/06/2017.
+ */
+public class IllegalCouncilPrivilegeQuantity extends Exception {
+    public IllegalCouncilPrivilegeQuantity (){
+        System.out.println("Council privilege with Quantity > 5 is not allowed");
+    }
+}
