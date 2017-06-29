2d2b72d6e0370e49000b4d3c1aa2d54f300c246d
Lacieoz <tommaso.tesser@mail.polimi.it>
2017-06-29 13:36:58 +0200

    fixes

--- src/main/java/it/polimi/ingsw/pc34/Controller/BooleanCreated.java
+++ src/main/java/it/polimi/ingsw/pc34/Controller/BooleanCreated.java
@@ -4,10 +4,10 @@ package it.polimi.ingsw.pc34.Controller;
  * Created by Povaz on 20/06/2017.
  */
 public class BooleanCreated {
-    private boolean choose;
+    private Boolean choose;
     private boolean available = false;
 
-    public synchronized boolean get() {
+    public synchronized Boolean get() {
         while(available == false) {
             try {
                 wait();
@@ -20,7 +20,7 @@ public class BooleanCreated {
         return choose;
     }
 
-    public synchronized void put(boolean choose) {
+    public synchronized void put(Boolean choose) {
         while (available == true) {
             try {
                 wait();
