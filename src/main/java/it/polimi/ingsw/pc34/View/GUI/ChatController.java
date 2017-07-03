941f737869b377375f17445b8d6bcc38009f4bd1
Erick <erick.venneri@mail.polimi.it>
2017-07-03 17:25:43 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ src/main/java/it/polimi/ingsw/pc34/View/GUI/ChatController.java
@@ -0,0 +1,51 @@
+package it.polimi.ingsw.pc34.View.GUI;
+
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import javax.xml.soap.Text;
+
+/**
+ * Created by Povaz on 03/07/2017.
+ */
+public class ChatController extends Application {
+    @FXML private Button sendMessageButton;
+    @FXML private TextField messageTextField;
+    @FXML private ScrollPane chatScrollPane;
+    @FXML private TextArea chatTextArea;
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("Chat.fxml"));
+        Scene scene = new Scene (root);
+
+        primaryStage.setTitle("La migliore Chat");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    @FXML private void initialize() {
+        chatScrollPane.setContent(chatTextArea);
+    }
+
+    @FXML public void messageClick() {
+        String message = messageTextField.getText();
+        messageTextField.clear();
+        if (message.equals("")) {
+            return;
+        }
+        else {
+            chatTextArea.appendText(message + "\n");
+        }
+    }
+}
