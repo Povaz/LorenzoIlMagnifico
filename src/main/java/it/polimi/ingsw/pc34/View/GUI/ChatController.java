73dc13dcd8954c1bee3875f3c135a1331af1831a
Paolo Trilli <paolo.trilli@mail.polimi.it>
2017-07-08 00:35:46 +0200

    Chat done

--- src/main/java/it/polimi/ingsw/pc34/View/GUI/ChatController.java
+++ src/main/java/it/polimi/ingsw/pc34/View/GUI/ChatController.java
@@ -1,16 +1,19 @@
 package it.polimi.ingsw.pc34.View.GUI;
 
+import com.sun.prism.paint.Paint;
 import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.TextAlignment;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
-import javax.xml.soap.Text;
 
 /**
  * Created by Povaz on 03/07/2017.
@@ -35,25 +38,43 @@ public class ChatController extends Application {
         primaryStage.show();
     }
 
-    @FXML private void initialize() {
-
+    @FXML private void enterPressed(KeyEvent event){
+        if(event.getCode() == KeyCode.ENTER){
+            messageClick();
+        }
     }
 
     @FXML public void messageClick() {
-        String message = messageTextField.getText();
-        messageTextField.clear();
-        if (message.equals("")) {
+        if(messageTextField.getText().equals("")){
             return;
         }
-        else {
-            Label newMessageLabel = new Label(message + "\n" + " ");
-            newMessageLabel.setWrapText(true);
-            newMessageLabel.setTextAlignment(TextAlignment.JUSTIFY);
-            chatVBOX.getChildren().add(newMessageLabel);
+
+        String message = "/chat" + "username" + "/: " + messageTextField.getText();
+        messageTextField.clear();
+
+
+        message = "/chatpaoloTrilli:ciaosonopaoloefacciolepuzzette,masoloneigiornidisparimasoloneigiornidispari";
+
+        String received = message;
+        if(message.startsWith("/error")){
+            Text error = new Text(message.substring(6));
+            error.setFill(Color.RED);
+            error.setFont(new Font("System Bold", 15));
+            error.setWrappingWidth(490);
+            chatVBOX.getChildren().add(error);
+        }
+        else if(message.startsWith("/chat")){
+            Text text = new Text(message.substring(5));
+            text.setWrappingWidth(490);
+            chatVBOX.getChildren().add(text);
         }
     }
 
     public void setMain(Main main){
         this.main = main;
+    }
+    
+    public static void main(String[] args){
+    	launch(ChatController.class);
     }
 }
